578fc2c535b0dfba2cb11807b82ef2a8f130131b
krishnaShreedhar <krishna.shreedhar@gmail.com>
2021-11-29 18:22:05 -0500

    add more structure

--- src/ensemble.py
+++ src/ensemble.py
@@ -1,7 +1,12 @@
+import argparse
+import os
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import Sequential
+import pandas as pd
 
+
+import constants
 
 def get_intermediate_output_1(model, data, layer_name='my_layer'):
     extractor = keras.Model(inputs=model.inputs,
@@ -52,10 +57,36 @@ def load_models(list_paths):
     return list_models
 
 
-def main():
+def create_ensembled_features(args):
     list_paths = []
     list_models = load_models(list_paths)
-    get_varied_features()
+    list_records = get_varied_features()
+
+    df_ensemble = pd.DataFrame.from_records(list_records)
+
+    out_path = os.path.join(constants.DIR_OUTPUTS, f"df_ensemble.csv")
+    df_ensemble.to_csv(out_path, index=False)
+
+
+def train_ensembled_models(args):
+    list_models = []
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("r")
+
+    args = parser.parse_args()
+    return args
+
+
+
+
+
+def main():
+    args = parse_args()
+    create_ensembled_features(args)
+
 
 
 if __name__ == '__main__':
