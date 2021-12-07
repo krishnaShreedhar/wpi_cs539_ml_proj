3a7223bcf7df0ad68e9baeba3f2db7223e41ced9
Shreedhar Kodate <krishna.shreedhar@gmail.com>
2021-12-07 15:42:56 -0500

    update the ensemble.py code to include data and model paths

--- src/ensemble.py
+++ src/ensemble.py
@@ -4,7 +4,9 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import Sequential
 import pandas as pd
+import numpy as np
 
+import utils
 import constants
 
 
@@ -22,45 +24,106 @@ def get_intermediate_output_2(model, data, layer_name='my_layer'):
     return intermediate_output
 
 
-def get_varied_features(list_models, data):
-    features = []
-    return features
+def get_varied_features(list_models, list_model_paths, data, list_data_paths):
+    list_all_features = []
+    layer_name = {"3dcnn": "dense_2", "resnet": "dense_2"}
+    layer_name = {"3dcnn": "dense_3", "resnet": "fc1"}
+
+    for d_index, dict_data in enumerate(list_data_paths):
+        dict_tmp = {
+            "data_id": list_data_paths[d_index]["data_id"],
+            "label": list_data_paths[d_index]["label"]
+        }
+        for index, model in enumerate(list_models):
+            dense_3 = get_intermediate_output_1(model, data, layer_name="dense_3")
+            dense_2 = get_intermediate_output_1(model, data, layer_name="dense_2")
+            dict_tmp["dense_3"] = dense_3
+            dict_tmp["dense_2"] = dense_2
+            dict_tmp["mri_type"] = list_model_paths[index]["mri_type"]
+
+        list_all_features.append(dict_tmp)
+    return list_all_features
 
 
-def get_features():
-    model = Sequential([
-        layers.Conv2D(32, 3, activation='relu'),
-        layers.Conv2D(32, 3, activation='relu'),
-        layers.MaxPooling2D(2),
-        layers.Conv2D(32, 3, activation='relu'),
-        layers.Conv2D(32, 3, activation='relu'),
-        layers.GlobalMaxPooling2D(),
-        layers.Dense(10),
-    ])
-    list_models = [model, model]
-    data = []
-    get_varied_features(list_models, data)
+# def get_features(model, data):
+#     model = Sequential([
+#         layers.Conv2D(32, 3, activation='relu'),
+#         layers.Conv2D(32, 3, activation='relu'),
+#         layers.MaxPooling2D(2),
+#         layers.Conv2D(32, 3, activation='relu'),
+#         layers.Conv2D(32, 3, activation='relu'),
+#         layers.GlobalMaxPooling2D(),
+#         layers.Dense(10),
+#     ])
+#     list_models = [model, model]
+#     data = []
+#     get_varied_features(list_models, data)
 
 
 def _load_model(model_path, **kwargs):
-    # TODO: Add load model code
-    model = []
+    model = keras.models.load_model(model_path)
+    print(model.summary())
     return model
 
 
 def load_models(list_paths):
     list_models = []
-    for index, model_path in enumerate(list_paths):
-        model = _load_model(model_path)
+    for index, dict_model_path in enumerate(list_paths):
+        model = _load_model(dict_model_path["path"])
         list_models.append(model)
 
     return list_models
 
 
+def get_data(list_data):
+    patient_scans_test = np.array([utils.process_scan(path) for path in list_data])
+    return patient_scans_test
+
+
+def get_list_model_paths():
+    list_model_paths = [
+        {
+            "path": "models/3d_image_classification_simple_T1w_1.h5",
+            "model_type": "3dcnn",
+            "mri_type": "T1w"
+        },
+        {
+            "path": "models/3d_image_classification_simple_T2w_1.h5",
+            "model_type": "3dcnn",
+            "mri_type": "T2w"
+        }
+    ]
+    return list_model_paths
+
+
+def get_list_data_paths():
+    list_data_paths = [
+        {
+            "data_id": "00001",
+            "path": "models/3d_image_classification_simple_T1w_1.h5",
+            "label": 0
+        },
+        {
+            "data_id": "00001",
+            "path": "models/3d_image_classification_simple_T1w_1.h5",
+            "label": 0
+        },
+        {
+            "data_id": "00001",
+            "path": "models/3d_image_classification_simple_T2w_1.h5",
+            "label": 1
+        }
+    ]
+    return list_data_paths
+
+
 def create_ensembled_features(args):
-    list_paths = []
-    list_models = load_models(list_paths)
-    list_records = get_varied_features()
+    list_model_paths = get_list_model_paths()
+    list_data_paths = get_list_data_paths()
+
+    list_models = load_models(list_model_paths)
+    data = get_data(list_data_paths)
+    list_records = get_varied_features(list_models, list_model_paths, data, list_data_paths)
 
     df_ensemble = pd.DataFrame.from_records(list_records)
 
@@ -74,7 +137,7 @@ def train_ensembled_models(args):
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("r")
+    # parser.add_argument("r")
 
     args = parser.parse_args()
     return args
