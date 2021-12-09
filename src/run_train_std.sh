6f253334bb0f1434421c4e419597612825ef2ffa
Shreedhar Kodate <krishna.shreedhar@gmail.com>
2021-12-09 00:37:08 -0500

    update run_train_std.sh

--- src/run_train_std.sh
+++ src/run_train_std.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH --job-name=ml_param
+#SBATCH --job-name=mri_std
 #SBATCH -t 54:00:00
 #SBATCH -C A100
 #SBATCH --mem 80G
@@ -12,7 +12,13 @@ module load cudnn/8.1.1.33-11.2/3k5bbs63
 
 source /home/sskodate/py37_venv/bin/activate
 
-echo "Starting to run SBATCH SCRIPT FOLD 3"
+echo "Starting to run mri_std_nw"
 
-python3 mri_std_nw.py --model_to_train=3dcnn --epochs=1 --initial_lr=0.01 --decay_steps=100000 --decay_rate=0.96 --patience=5 --verbose=2 --w_width=192 --h_height=192 --d_depth=60 --classes=2 --mri_type=T1w --max_data=10 --batch_size=2
-python3 mri_std_nw.py --model_to_train=resnet --epochs=1 --initial_lr=0.01 --decay_steps=100000 --decay_rate=0.96 --patience=5 --verbose=2 --w_width=192 --h_height=192 --d_depth=60 --classes=2 --mri_type=T1w --max_data=10 --batch_size=2
+python3 mri_std_nw.py --model_to_train=3dcnn --epochs=50 --initial_lr=0.01 --decay_steps=100000 --decay_rate=0.96 --patience=50 --verbose=2 --w_width=192 --h_height=192 --d_depth=60 --classes=2 --mri_type=T1w --max_data=1000 --batch_size=2
+python3 mri_std_nw.py --model_to_train=resnet --epochs=50 --initial_lr=0.01 --decay_steps=100000 --decay_rate=0.96 --patience=50 --verbose=2 --w_width=192 --h_height=192 --d_depth=60 --classes=2 --mri_type=T1w --max_data=1000 --batch_size=2
+python3 mri_std_nw.py --model_to_train=3dcnn --epochs=50 --initial_lr=0.01 --decay_steps=100000 --decay_rate=0.96 --patience=50 --verbose=2 --w_width=192 --h_height=192 --d_depth=60 --classes=2 --mri_type=T2w --max_data=1000 --batch_size=2
+python3 mri_std_nw.py --model_to_train=resnet --epochs=50 --initial_lr=0.01 --decay_steps=100000 --decay_rate=0.96 --patience=50 --verbose=2 --w_width=192 --h_height=192 --d_depth=60 --classes=2 --mri_type=T2w --max_data=1000 --batch_size=2
+python3 mri_std_nw.py --model_to_train=3dcnn --epochs=50 --initial_lr=0.01 --decay_steps=100000 --decay_rate=0.96 --patience=50 --verbose=2 --w_width=192 --h_height=192 --d_depth=60 --classes=2 --mri_type=T1wCE --max_data=1000 --batch_size=2
+python3 mri_std_nw.py --model_to_train=resnet --epochs=50 --initial_lr=0.01 --decay_steps=100000 --decay_rate=0.96 --patience=50 --verbose=2 --w_width=192 --h_height=192 --d_depth=60 --classes=2 --mri_type=T1wCE --max_data=1000 --batch_size=2
+python3 mri_std_nw.py --model_to_train=3dcnn --epochs=50 --initial_lr=0.01 --decay_steps=100000 --decay_rate=0.96 --patience=50 --verbose=2 --w_width=192 --h_height=192 --d_depth=60 --classes=2 --mri_type=FLAIR --max_data=1000 --batch_size=2
+python3 mri_std_nw.py --model_to_train=resnet --epochs=50 --initial_lr=0.01 --decay_steps=100000 --decay_rate=0.96 --patience=50 --verbose=2 --w_width=192 --h_height=192 --d_depth=60 --classes=2 --mri_type=FLAIR --max_data=1000 --batch_size=2
